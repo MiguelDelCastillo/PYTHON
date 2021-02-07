f9ddf041bb8409f21bec4793f7c016d3a9892cd8
MiguelDelCastillo <47579615+MiguelDelCastillo@users.noreply.github.com>
2021-02-07 13:30:04 -0600

    Update primerTarea.py

--- primerTarea.py
+++ primerTarea.py
@@ -1,11 +1,11 @@
 print("Bienvenido al programa")
-#Datos a pedir
+#Datos con su tipo de datos
 velocidadcoche = float(input('Cual es la velocidad del coche?'))
 presupuestocoche = float(input("Cual es tu presupuesto del coche?"))
 precioxlitro = float(input("Cuanto cuesta el litro de gasolina?"))
 rendimiento = float(input("Cuanto rinde por litro el coche? "))
 
-    #Operaciones a realizar
+#Operaciones a realizar
 litrosdisponibles = presupuestocoche / precioxlitro
 distancia = litrosdisponibles * rendimiento
 tiempo = velocidadcoche / distancia
